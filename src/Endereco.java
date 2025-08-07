7792fbaedd7cc18e2aa21d68d5a5832843c606be
Vitor Francisco Schmitt <vitorfran5@gmail.com>
2025-08-07 20:17:24 -0300

    criado a classe endereço.java

--- src/Endereco.java
+++ src/Endereco.java
@@ -1,4 +1,91 @@
 
 public class Endereco {
 
+	private Long id;
+	private String logradouro;
+	private String numero;
+	private String complemento;
+	private String bairro;
+	private String cidade;
+	private String estado;
+	private String cep;
+
+	public Endereco(Long id, String numero, String complemento, String bairro, String cidade, String estado, String cep,
+			String logradouro){
+
+		setId(id);
+		setLogradouro(logradouro);
+		setNumero(numero);
+		setComplemento(complemento);
+		setBairro(bairro);
+		setCidade(cidade);
+		setEstado(estado);
+		setCep(cep);
+
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getLogradouro() {
+		return logradouro;
+	}
+
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
 }
