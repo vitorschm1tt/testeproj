aa8b5f97abd16eccf6d7de622e1af32badf8495c
Vitor Francisco Schmitt <vitorfran5@gmail.com>
2025-08-08 19:29:41 -0300

    criado a classe Especie.java

---
+++ src/Especie.java
@@ -0,0 +1,27 @@
+
+public class Especie {
+
+	private Long id;
+	private String nomeEspecie;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNomeEspecie() {
+		return nomeEspecie;
+	}
+
+	public void setNomeEspecie(String nomeEspecie) {
+		this.nomeEspecie = nomeEspecie;
+	}
+
+	public Especie(Long id, String nomeEspecie) {
+
+	}
+
+}
