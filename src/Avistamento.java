756e87d68be513f5a2b0f34b68c6e4bf1723f44b
Vitor Francisco Schmitt <vitorfran5@gmail.com>
2025-08-11 19:30:00 -0300

    Criado a classe Avistamento.java

---
+++ src/Avistamento.java
@@ -0,0 +1,18 @@
+
+public class Avistamento {
+
+	private Long id;
+	
+	public Avistamento(Long id) {
+		setId(id);
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+}
