3c81825587a68eaa4d6b5259fe23f4103bbad2f0
Vitor Francisco Schmitt <vitorfran5@gmail.com>
2025-08-11 19:37:58 -0300

    Criado a classe Resgate.java

---
+++ src/Resgate.java
@@ -0,0 +1,18 @@
+
+public class Resgate {
+
+	private Long id;
+
+	public Resgate(Long id) {
+		setId(id);
+
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+}
