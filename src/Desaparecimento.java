2906745226a7f9acf06aef25c61f10bd4a6d3295
Vitor Francisco Schmitt <vitorfran5@gmail.com>
2025-08-11 19:26:44 -0300

    Criado a classe Desaparecimento.java

---
+++ src/Desaparecimento.java
@@ -0,0 +1,18 @@
+
+public class Desaparecimento {
+
+	private Long id;
+	
+	public Desaparecimento( Long id) {
+		setId(id);
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+}
